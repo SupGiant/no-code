eae92acaa6cbd0622ba6692524ba506774b21ae3
linr <linr@bo-en.com>
2023-08-20 10:28:38 +0800

    refactor: 引入knife4j接口文档，可以访问localhost:8080/doc.html，也可以访问原生的swagger-ui.html

--- moyu-module-system/moyu-module-system-biz/src/main/java/org/moyu/controller/admin/user/UserController.java
+++ moyu-module-system/moyu-module-system-biz/src/main/java/org/moyu/controller/admin/user/UserController.java
@@ -1,5 +1,7 @@
 package org.moyu.controller.admin.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.moyu.service.user.AdminUserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/system/user")
+@Tag(name = "管理后台 - 用户")
 public class UserController {
 
     @Resource
     private AdminUserService adminUserService;
 
     @GetMapping("/test")
+    @Operation(summary = "新增用户")
     String test(){
         boolean userTest = adminUserService.createUserTest();
         return userTest?"插入成功":"插入失败";
